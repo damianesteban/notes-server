93fa5000bf83be7d13008b10e91fb32d5f41ffe6
Damian Esteban <damian.esteban@gmail.com>
2016-12-15 22:00:52 -0500

    Fix authrouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,14 +2,12 @@ const express = require('express');
 const jwt = require("jsonwebtoken");
 const authRouter = express.Router();
 
-
 authRouter.post("/authorization", (req, res) => {
   let user = req.body;
   if (user.username === "valid_user" && user.password === "password") {
     let token = jwt.sign(user, "super_secret_key", {
       expiresIn: 60 
     });
-    
     res.status(200).send({ username: user.username, token: token });
   } else {
     res.status(401).send({ message: 'Authentication failed.' });
