2fe6f034d00d402d11a29e3b54b5c3eb80f0717c
Damian Esteban <damian.esteban@gmail.com>
2016-12-21 19:18:48 -0500

    Update notes endpoint

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -2,18 +2,18 @@ const express = require('express');
 const notesRouter = express.Router();
 
 const Note = require('../models/note');
-const NoteController = require('../controllers/notecontroller.js')//(Note);
+const NoteController = require('../controllers/notecontroller.js');
 
 // Get all Notes
-notesRouter.get('/note', NoteController.GetNote);
+notesRouter.get('/notes', NoteController.GetNote);
 
 // Create new Note
-notesRouter.post('/note', NoteController.PostNote);
+notesRouter.post('/notes', NoteController.PostNote);
 
 // Delete a Note based on :id
-notesRouter.delete('/note/:id', NoteController.DeleteNote);
+notesRouter.delete('/notes/:id', NoteController.DeleteNote);
 
 // Update a Note based on :id
-notesRouter.put('/note/:id', NoteController.UpdateNote);
+notesRouter.put('/notes/:id', NoteController.UpdateNote);
 
 module.exports = notesRouter;
