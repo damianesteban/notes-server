62c787af8f840f80ba09e421419be7705295fcb0
Damian Esteban <damian.esteban@gmail.com>
2016-12-22 23:30:55 -0500

    Update status codes

--- controllers/notecontroller.js
+++ controllers/notecontroller.js
@@ -5,10 +5,10 @@ const Note = require('../models/note');
         GetNote: function(req, res){
             Note.find({}, function(err, notes){
               if(err) {
-                res.json({status: false, error: "Something went wrong"});
+                res.json({result: {success: false, error: "Something went wrong"}});
                 return;
               }
-              res.json({status: true, note: notes});
+              res.status(200).json({success: true, result: notes});
             });
         },
         //Post a Note into Database
@@ -16,10 +16,10 @@ const Note = require('../models/note');
            const note = new Note(req.body);
            note.save(function(err, note){
               if(err) {
-                res.json({status: false, error: "Something went wrong"});
+                res.json({success: false, error: "Something went wrong"});
                 return;
               }
-              res.json({status: true, message: "Note Saved!!"});
+              res.status(201).json({result: {success: true}});
             });
         },
         //Updating a Note status based on an ID
@@ -29,20 +29,20 @@ const Note = require('../models/note');
             Note.completed = completed;
             Note.save(function(err, note){
               if(err) {
-                res.json({status: false, error: "Status not updated"});
+                res.json({success: false, error: "Status not updated"});
               }
-              res.json({status: true, message: "Status updated successfully"});
-            });
+              res.status(204).json({result: {success: true}});
+              });
             });
         },
         // Deleting a Note baed on an ID
         DeleteNote: function(req, res){
           Note.remove({_id: req.params.id}, function(err, notes){
             if(err) {
-              res.json({status: false, error: "Deleting Note is not successfull"});
+              res.json({success: false, error: "Deleting Note is not successfull"});
               return;
             }
-            res.json({status: true, message: "Note deleted successfully!!"});
+            res.status(204).json({result:{success: true}});
           });
         }
     }
